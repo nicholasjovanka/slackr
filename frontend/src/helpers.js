51dcfd60d4816d8ea92b4794e37a6479f981abef
nicholasjovanka <43334334+nicholasjovanka@users.noreply.github.com>
2023-10-11 22:37:43 +1100

    Register Page

--- frontend/src/helpers.js
+++ frontend/src/helpers.js
@@ -8,7 +8,7 @@
  *   https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URIs
  * 
  * Example Usage:
- *   const file = document.querySelector('input[type="file"]').files[0];
+ *   const file = document.querySelector('input[type='file']').files[0];
  *   console.log(fileToDataUrl(file));
  * @param {File} file The file to be read.
  * @return {Promise<string>} Promise which resolves to the file as a data url.
@@ -29,3 +29,53 @@ export function fileToDataUrl(file) {
     reader.readAsDataURL(file);
     return dataUrlPromise;
 }
+
+export const movePage = (currentPage, destinationPage) => {
+    let currentPageElement = document.getElementById(currentPage);
+    let destinationPageElement = document.getElementById(destinationPage);
+    destinationPageElement.classList.remove('d-none');
+    currentPageElement.classList.add('d-none')
+}
+
+export const showWarningModal = (title,message) => {
+    let modalHeader = document.getElementById('warning-modal-title');
+    let modalBody = document.getElementById('warning-modal-body');
+    modalHeader.innerHTML = title;
+    modalBody.innerHTML = message;
+    let modal = document.getElementById('warning-modal');
+    const myModal = new bootstrap.Modal(modal)
+    myModal.show();
+}
+
+export const checkEmail = (email) => {
+    let regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@']+(\.[^<>()\[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    return regexp.test(email.trim());
+}
+
+export const formInputElementValidator = (inputElement, isValid) => {
+    if(isValid){
+        inputElement.classList.remove('is-invalid');
+        inputElement.classList.add('is-valid');
+    } else {
+        inputElement.classList.remove('is-valid');
+        inputElement.classList.add('is-invalid');
+    }
+}
+
+export const emptyInputValidator = (inputElement,warningElement,fieldName ,trim = true) => {
+    let inputValue = trim?inputElement.value.trim():inputElement.value;
+    if(inputValue.length === 0){
+        if(warningElement !== null){
+            warningElement.innerHTML = `${fieldName} cannot be empty`
+        }
+        formInputElementValidator(inputElement, false);
+        return true;
+    } else {
+        formInputElementValidator(inputElement, true);
+        return false;
+    }
+}
+
+// export const checkValidPasswordInput = (inputElement1, inputElement2) => {
+//     if(i)
+// }
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,110 @@
 import { BACKEND_PORT } from './config.js';
 // A helper you may want to use when uploading new images to the server.
-import { fileToDataUrl } from './helpers.js';
+import { fileToDataUrl, showWarningModal,movePage,checkEmail, formInputElementValidator, emptyInputValidator } from './helpers.js';
+
 
 console.log('Let\'s go!');
+
+
+//Login page
+let loginPageRegisterButton = document.getElementById('login-page-register-button');
+
+loginPageRegisterButton.addEventListener('click', (e) => {
+    movePage('login-page', 'register-page');
+})
+
+
+
+
+//Register Page
+
+let registerNameInput = document.getElementById('register-name-input');
+
+const validateRegisterPageNameInput = () => {
+    let registerNameWarningDiv = document.getElementById('register-name-invalid-message');
+    emptyInputValidator(registerNameInput,registerNameWarningDiv,"Name");
+}
+
+registerNameInput.addEventListener('blur', (e) => {
+    validateRegisterPageNameInput();
+})
+
+
+let registerPageEmailInput = document.getElementById('register-email-input');
+
+const validateRegisterPageEmailInput = () => {
+    let registerEmailWarningDiv = document.getElementById('register-email-invalid-message');
+    let isEmpty = emptyInputValidator(registerPageEmailInput,registerEmailWarningDiv,"Email");
+    if(!isEmpty) {
+        let emailIsValid = checkEmail(registerPageEmailInput.value);
+        if (!emailIsValid) {
+            registerEmailWarningDiv.innerHTML = "Wrong Email Format";
+        }
+        formInputElementValidator(registerPageEmailInput, emailIsValid);
+    }
+}
+
+registerPageEmailInput.addEventListener('blur', (e) => {
+    validateRegisterPageEmailInput();
+})
+
+let registerPasswordInput = document.getElementById('register-password-input');
+let registerConfirmPasswordInput = document.getElementById('register-confirm-password-input');
+
+const validateRegisterPagePasswordInput = () => {
+    let registerPasswordWarningDiv = document.getElementById('register-password-invalid-message');
+    let valid = true;
+    if(registerPasswordInput.value.length === 0 || registerConfirmPasswordInput.value.length === 0){
+        registerPasswordWarningDiv.innerHTML = `${registerPasswordInput.value.length === 0?'Password':'Confirm Password'} field cannot be empty`
+        valid = false;
+    } else {
+        if(registerPasswordInput.value === registerConfirmPasswordInput.value) {
+            valid = true;
+        } else {
+            valid = false;
+            registerPasswordWarningDiv.innerHTML = "Password must be the same"
+        }
+    }
+    formInputElementValidator(registerPasswordInput, valid);
+    formInputElementValidator(registerConfirmPasswordInput, valid);
+
+}
+
+registerPasswordInput.addEventListener('blur', (e) => {
+    validateRegisterPagePasswordInput();
+})
+
+registerConfirmPasswordInput.addEventListener('blur', (e) => {
+    validateRegisterPagePasswordInput();
+})
+
+
+let registerPageLoginHyperLink = document.getElementById('register-page-a-element');
+
+registerPageLoginHyperLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    movePage('register-page','login-page');
+})
+
+
+let registerPageButton = document.getElementById('register-page-submit-button');
+registerPageButton.addEventListener('click', (e) => {
+    validateRegisterPageNameInput();
+    validateRegisterPageEmailInput();
+    validateRegisterPagePasswordInput();
+    // e.preventDefault();
+    // showWarningModal('Error','SOMETHING');
+})
+
+
+
+
+
+
+
+let loginForm = document.getElementById('registration-form');
+
+loginForm.addEventListener('submit',(e) => {
+    e.preventDefault();
+    e.stopPropagation();
+}, false)
